bc267dfb2ccae2e36328201669c9b441b7c5ac81
Daniel Pollock <daniellpollock@gmail.com>
2013-07-30 21:12:53 -0500

    Added Sponsors controller

---
+++ LRDNUG/LRDNUG.Web/Controllers/SponsorController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LRDNUG.Web.Controllers
+{
+    public class SponsorController : BaseController
+    {
+        public ActionResult Index()
+        {
+            var sponsors = DBContext.Sponsors.ToList();
+            return View(sponsors);
+        }
+
+    }
+}
